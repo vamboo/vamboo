2ea4d48c2a9bea0f19485bfaa49468ad9c7a1e5e
wh11e <wh11e7rue@icloud.com>
2017-07-03 18:54:26 +0900

    It works

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as ReactDOM from 'react-dom'
+import * as h from 'react-hyperscript'
+
+ReactDOM.render(
+  h('h1', 'UNDER CONSTRUCTION'),
+  document.querySelector('body > *')
+)
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {LoaderOptionsPlugin} = require('webpack')
+
+module.exports = {
+  entry: [
+    './src/index.ts'
+  ],
+  output: {
+    path: __dirname + '/static',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        exclude: /node_modules/,
+        use: 'pug-loader'
+      },
+      {
+        test: /\.ts$/,
+        exclude: '/node_modules/',
+        use: 'ts-loader'
+      },
+      {
+        test: /\.styl$/,
+        exclude: '/node_modules/',
+        use: ['style-loader', 'css-loader', 'stylus-loader']
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './static'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    }),
+    new LoaderOptionsPlugin({
+      options: {
+        stylus: {
+          use: [require('poststylus')(['autoprefixer'])]
+        }
+      }
+    })
+  ]
+
+}
