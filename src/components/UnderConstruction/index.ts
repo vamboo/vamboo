a7a003c111789814c3856b3e5df30333ea0b6807
wh11e <wh11e7rue@icloud.com>
2017-07-17 21:28:32 +0900

    Change directory structure

--- src/components/UnderConstruction/index.ts
+++ src/components/UnderConstruction/index.ts
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as h from 'react-hyperscript'
 import * as CSSModules from 'react-css-modules'
-import * as styles from './UnderConstruction.styl'
+import * as styles from './style.styl'
 
 
 @CSSModules(styles)
