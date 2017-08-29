d97b0afe0241270757b7482546385953da30b6ea
Sosuke Suzuki <aosukeke@gmail.com>
2017-08-30 02:48:52 +0900

    remove failed import

--- src/components/BlockList/GUIBlockComponents/SliderBlockComponent.ts
+++ src/components/BlockList/GUIBlockComponents/SliderBlockComponent.ts
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import * as h from 'react-hyperscript'
-import GUIBlock from '../../../lib/blocks/BaseBlock'
 
 export default class SliderBlockComponent extends React.Component {
     render() {
