8975cf64abe4a064a6a02fc2d24661fa829f51bb
Sosuke Suzuki <aosukeke@gmail.com>
2017-08-30 02:02:40 +0900

    create a MainComponent

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import * as ReactDOM from 'react-dom'
 import * as h from 'react-hyperscript'
-import UnderConstruction from './components/UnderConstruction'
-import BlockList from './components/BlockList/BlockTabs'
+import MainComponent from './components/MainComponent'
 
 ReactDOM.render(
-  h(BlockList),
+  h(MainComponent),
   document.querySelector('body > *')
 )
