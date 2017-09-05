532e2cb93baa88bbc627701376e5237d95875a46
wh11e <wh11e7rue@icloud.com>
2017-09-06 01:56:23 +0900

    Implement BlocksEnvironment

---
+++ src/lib/BlocksEnvironment.ts
@@ -0,0 +1,23 @@
+import * as _ from 'lodash'
+import {default as BaseBlock, FunctionBlock, GUIElementBlock, SourceBlock, SinkBlock} from './blocks/BaseBlock'
+
+
+class BlocksEnvironment {
+  private blocks: typeof BaseBlock[]
+
+  register(block: typeof BaseBlock) {
+    this.blocks.push(block)
+  }
+
+  unregister(block: typeof BaseBlock) {
+    _.pull(this.blocks, block)
+  }
+
+  getBlocks(blockKind?: typeof FunctionBlock | typeof GUIElementBlock | typeof SourceBlock | typeof SinkBlock) {
+    if (blockKind === undefined) {
+      return this.blocks
+    }
+
+    return this.blocks.filter(block => Object.getPrototypeOf(block) === blockKind)
+  }
+}
