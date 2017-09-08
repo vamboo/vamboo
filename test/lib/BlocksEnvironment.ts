31af1f39ec58112c3f0e5c0399fff6cd0b00c772
wh11e <wh11e7rue@icloud.com>
2017-09-08 20:01:25 +0900

    Add test about BlocksEnvironment#getBlocks

--- test/lib/BlocksEnvironment.ts
+++ test/lib/BlocksEnvironment.ts
@@ -1,5 +1,5 @@
 import test from 'ava'
-import {FunctionBlock} from '../../src/lib/blocks/BaseBlock'
+import {FunctionBlock, GUIElementBlock, SourceBlock, SinkBlock} from '../../src/lib/blocks/BaseBlock'
 import BlocksEnvironment from '../../src/lib/BlocksEnvironment'
 
 
@@ -36,4 +36,61 @@ test('register block using decorator', t => {
   }
 
   t.deepEqual(blocksEnvironment.getBlocks(), [StubBlock2])
+})
+
+test('filter by block kind', t => {
+  const blocksEnvironment = new BlocksEnvironment
+
+  @blocksEnvironment.register
+  class StubFunctionBlock extends FunctionBlock {
+    static blockName = 'Stub function'
+
+    inputs = []
+    outputs = []
+
+    onInputUpdate() {
+      throw 'not implemented'
+    }
+  }
+
+  @blocksEnvironment.register
+  class StubGUIElementBlock extends GUIElementBlock {
+    static blockName = 'Stub GUI Element'
+
+    inputs = []
+    outputs = []
+
+    onInputUpdate() {
+      throw 'not implemented'
+    }
+  }
+
+  @blocksEnvironment.register
+  class StubSourceBlock extends SourceBlock {
+    static blockName = 'Stub source'
+
+    inputs = []
+    outputs = []
+
+    onInputUpdate() {
+      throw 'not implemented'
+    }
+  }
+
+  @blocksEnvironment.register
+  class StubSinkBlock extends SinkBlock {
+    static blockName = 'Stub sink'
+
+    inputs = []
+    outputs = []
+
+    onInputUpdate() {
+      throw 'not implemented'
+    }
+  }
+
+  t.deepEqual(blocksEnvironment.getBlocks(FunctionBlock), [StubFunctionBlock])
+  t.deepEqual(blocksEnvironment.getBlocks(GUIElementBlock), [StubGUIElementBlock])
+  t.deepEqual(blocksEnvironment.getBlocks(SourceBlock), [StubSourceBlock])
+  t.deepEqual(blocksEnvironment.getBlocks(SinkBlock), [StubSinkBlock])
 })
