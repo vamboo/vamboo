d451e010215e3f1bbffcd6ff469382ba974dcdfc
wh11e <wh11e7rue@icloud.com>
2017-09-13 23:15:34 +0900

    Setup story for InputProperty component

--- stories/index.ts
+++ stories/index.ts
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import * as h from 'react-hyperscript'
-import UnderConstruction from '../src/components/UnderConstruction'
+import Input from '../src/lib/Input'
+import Output from '../src/lib/Output'
+import BaseBlock from '../src/lib/blocks/BaseBlock'
+import InputProperty from '../src/components/InputProperty'
 
 
-storiesOf('Button', module)
-  .add('with text', () => h(UnderConstruction))
+class DummyBlock extends BaseBlock {
+  onInputUpdate() {}
+}
+
+
+storiesOf('InputProperty', module)
+  .add('not connected', () => h(InputProperty, {input: new Input<number>('input1', new DummyBlock)}))
+  .add('connected', () => {
+    const input = new Input<number>('input2', new DummyBlock)
+    input.connect(new Output<number>('output2', 0))
+    return h(InputProperty, {input})
+  })
