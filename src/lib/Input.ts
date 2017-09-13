0310e766ebc408ac69fd82f5cc0ad393fc0dfc31
wh11e <wh11e7rue@icloud.com>
2017-09-13 23:00:11 +0900

    Make it able to set handler of when Output connected to Input is changed

--- src/lib/Input.ts
+++ src/lib/Input.ts
@@ -1,16 +1,21 @@
 import Observer from './Observer'
 import Output from './Output'
 import BaseBlock from './blocks/BaseBlock'
+import Observable from './Observable'
 
 
 export default class Input<T> {
-  output?: Output<T>;
+  connectionObservable: Observable<Output<T> | null> = new Observable(null)
 
   constructor(public name: string, public block: BaseBlock) {}
 
   connect(output: Output<T>) {
-    this.output = output
+    this.connectionObservable.value = output
     output.observable.addObserver(this.block.onInputUpdate.bind(this.block))
+  }
+
+  get output(): Output<T> | null {
+    return this.connectionObservable.value
   }
 
   get value() {
