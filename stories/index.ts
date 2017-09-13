dffcad9421372fb4592bdcd29bd08dab07757986
wh11e <wh11e7rue@icloud.com>
2017-09-14 02:43:17 +0900

    Setup a story for InputProperties

--- stories/index.ts
+++ stories/index.ts
@@ -5,6 +5,7 @@ import Input from '../src/lib/Input'
 import Output from '../src/lib/Output'
 import BaseBlock from '../src/lib/blocks/BaseBlock'
 import InputProperty from '../src/components/InputProperty'
+import InputProperties from '../src/components/InputProperties'
 
 
 class DummyBlock extends BaseBlock {
@@ -18,4 +19,13 @@ storiesOf('InputProperty', module)
     const input = new Input<number>('input2', new DummyBlock)
     input.connect(new Output<number>('output2', 0))
     return h(InputProperty, {input})
+  })
+
+storiesOf('InputProperties', module)
+  .add('foo', () => {
+    const block = new DummyBlock
+    const input1 = new Input<number>('input1', block)
+    input1.connect(new Output<number>('output1', 0))
+    const input2 = new Input<number>('input2', block)
+    return h(InputProperties, {inputs: [input1, input2]})
   })
