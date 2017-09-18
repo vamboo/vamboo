78cb24f4fc9953b758b65c8a69eaea13755fdeac
Sosuke Suzuki <aosukeke@gmail.com>
2017-09-19 00:55:29 +0900

    fix directory structure for GUIElement

--- src/components/BlockList/GUIBlockList.ts
+++ src/components/BlockList/GUIBlockList.ts
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as h from 'react-hyperscript'
-import SliderBlockElement from './GUIBlockElements/SliderBlockElement'
-import LabelBlockElement from './GUIBlockElements/LabelBlockElement'
+import SliderBlockElement from '../GUIElements/SliderElement'
+import LabelBlockElement from '../GUIElements/LabelElement'
 
 export default class GUIBlockList extends React.Component {
   render() {
