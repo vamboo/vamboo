0400d4c79ddb11cd1ee89e67c83c94eaf1f642c4
tock203 <tick@tock.tech>
2017-10-18 07:27:31 +0900

    Fix SliderBlock bug

--- src/components/Slider/index.ts
+++ src/components/Slider/index.ts
@@ -13,6 +13,6 @@ export default class extends React.Component<PropTypes> {
   }
 
   onChange(event: Event) {
-    this.props.block.outputs[0].value = (event.target as any).value
+    this.props.block.outputs[0].value = parseInt((event.target as any).value, 10)
   }
 }
