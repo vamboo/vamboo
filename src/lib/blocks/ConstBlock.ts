d7d73bf7fa424a76a73a0583830e870b82af16f8
tock203 <tick@tock.tech>
2017-10-21 04:33:52 +0900

    Make ConstBlock PushPullFunctionBlock

--- src/lib/blocks/ConstBlock.ts
+++ src/lib/blocks/ConstBlock.ts
@@ -1,25 +1,21 @@
-import {PushFunctionBlock} from './BaseBlock'
+import {PushPullFunctionBlock} from './BaseBlock'
 import Input from '../Input'
-import Output from '../Output'
+import {LazyOutput} from '../Output'
 
 
-// TODO: PushPullFunctionBlockにする
-export default class extends PushFunctionBlock {
+export default class extends PushPullFunctionBlock<number> {
   static blockName = '固定'
 
   inputs: Input<number>[] = [new Input<number>('A', 0, this), new Input<number>('B', 0, this)]
-  outputs: Output<number>[] = [new Output<number>('B', 0)]
+  outputs: [LazyOutput<number>] = [new LazyOutput<number>('B', 0, this)]
 
   constructor() {
     super()
 
-    this.inputs[0].connectionSubscription.subscribe(this.onInputUpdate.bind(this))
-    this.inputs[0].pushSubscription.subscribe(this.onInputUpdate.bind(this))
+    this.configure()
   }
 
-  onInputUpdate() {
-    if (this.inputs[1].pushSubscription.value !== null) {
-      this.outputs[0].push(this.inputs[1].pushSubscription.value!)
-    }
+  pull() {
+    return this.inputs[1].pushSubscription.value
   }
 }
