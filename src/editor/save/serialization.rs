fc088dda26c86162284c4deecbe3c928c8ece92e
tock203 <tick@tock.tech>
2018-09-24 12:29:20 +0900

    Deserialize package id in the format type.and.some.remainder

--- src/editor/save/serialization.rs
+++ src/editor/save/serialization.rs
@@ -1,8 +1,13 @@
+use std::fmt;
+use std::str::FromStr;
 extern crate serde;
 extern crate serde_json;
+extern crate failure;
+use serde::ser::{Serialize, Serializer};
+use serde::de::{Deserialize, Deserializer, Visitor};
+use failure::format_err;
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-#[serde(tag = "type")]
+#[derive(Serialize, Debug, PartialEq, Eq)]
 pub enum PackageId {
   BuiltIn,
   Local {
@@ -11,9 +16,51 @@ pub enum PackageId {
   Market {
     user: String,
     package: String
-  },
-  Git {
-    url: String
+  }
+}
+
+impl FromStr for PackageId {
+  type Err = failure::Error;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let mut segments = s.split(".");
+    let mut next_segment = || segments.next().ok_or(
+      format_err!("Package id does not have enough segments: {}", s)
+    );
+
+    let parsed = match next_segment()? {
+      "builtin" => PackageId::BuiltIn,
+      "local" => PackageId::Local {
+        package: next_segment()?.to_string()
+      },
+      "market" => PackageId::Market {
+        user: next_segment()?.to_string(),
+        package: next_segment()?.to_string()
+      },
+      _ => Err(format_err!("Package id does not have valid type: {}", s))?
+    };
+
+    Ok(parsed)
+  }
+}
+
+impl<'de> Deserialize<'de> for PackageId {
+  fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<PackageId, D::Error> {
+    deserializer.deserialize_str(PackageIdVisitor)
+  }
+}
+
+struct PackageIdVisitor;
+
+impl<'de> Visitor<'de> for PackageIdVisitor {
+  type Value = PackageId;
+
+  fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+    formatter.write_str("a package id")
+  }
+
+  fn visit_str<E: serde::de::Error>(self, value: &str) -> Result<Self::Value, E> {
+    value.parse::<PackageId>().map_err(serde::de::Error::custom)
   }
 }
 
