284f5a90a558e40bd6e7b7c0b3111f13c14f5fca
tock203 <tick@tock.tech>
2018-10-12 00:34:06 +0900

    My first proc_macro

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate proc_macro;
+use crate::proc_macro::TokenStream;
+
+extern crate syn;
+#[macro_use] extern crate quote;
+
+#[proc_macro_attribute]
+pub fn inject(_: TokenStream, input: TokenStream) -> TokenStream {
+  let item = syn::parse(input.clone()).expect("failed to parse");
+
+  let struct_name = match item {
+    syn::Item::Struct(struct_item) => struct_item.ident,
+    _ => panic!("expects struct")
+  };
+
+  let output = quote! {
+    impl #struct_name {
+      fn new() -> #struct_name {
+        #struct_name {}
+      }
     }
+  };
+
+  format!("{}{}", input, output).parse().unwrap()
 }
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,1 +1,10 @@
+extern crate compiler;
+
 pub mod stream;
+
+#[compiler::inject]
+struct ExecutablePackage {}
+
+fn hoge() -> ExecutablePackage {
+  ExecutablePackage::new()
+}
