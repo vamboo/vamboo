9fe4e181fda904392a7e41785b6f9669c6f07f98
tock203 <tick@tock.tech>
2019-03-13 15:34:31 +0900

    props/view

--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -1,8 +1,12 @@
 use frappe::{Sink, Stream};
-use frappe_dom::{initialize, mount, Element as El};
+use frappe_dom::{initialize, mount, Element as El, EventsSink};
 use maplit::hashmap;
+use maybe_owned::MaybeOwned;
+use petgraph::graph::{EdgeIndex, NodeIndex};
+use petgraph::{Directed, Graph};
+use std::ops::Deref;
 use wasm_bindgen::prelude::*;
-use web_sys::window;
+use web_sys::{window, MouseEvent};
 
 #[derive(Clone)]
 struct Function {
@@ -16,31 +20,130 @@ struct Main {
   argument: Option<Function>,
 }
 
-fn overview(main: Stream<Main>) -> Stream<El> {
-    main.map(move |main| {
-    El::new("div")
-      .attrs(hashmap! {
-        "style" => "margin: auto;"
-      })
-      .children(vec!["main".into()])
-  })
+// 多引数/多返り値を持つ関数を扱う際は()が引数<->返り値のマップになる
+#[derive(Clone)]
+struct CallGraph(Graph<String, (), Directed>);
+
+impl CallGraph {
+  fn new() -> Self {
+    let mut call_graph = Graph::new();
+    call_graph.add_node("main".to_string());
+    Self(call_graph)
+  }
 }
-//fn function_detail(function: Stream<Function>) -> Stream<El> {
-//  !
-//}
+
+impl Deref for CallGraph {
+  type Target = Graph<String, (), Directed>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+mod function_overview {
+  use super::*;
+
+  pub enum Msg {
+    FocusConnection(EdgeIndex),
+    FocusFunction(NodeIndex),
+    FocusArguments,
+    FocusReturns,
+  }
+
+  // call_graphがStreamである必要はないが、frappeがflatMapを持っていないので仕方なくStreamになっている
+  // TODO: frappeにflatMapを追加するPRを出す
+  // switchMapだったら所有権の問題もクリアできそう？
+  pub(super) fn view(call_graph: Stream<CallGraph>) -> (Stream<El>, Stream<Msg>) {
+    let mut returns = EventsSink::new();
+    let msg = returns
+      .events("click")
+      .map(|_: MaybeOwned<MouseEvent>| Msg::FocusReturns);
+
+    // TODO: call_graphを使って各GUI要素との繋がりを表示する
+    let view = call_graph.map(move |_| {
+      El::new("div")
+        .children(vec![
+          El::new("div")
+            .attrs(hashmap! {
+              "style" => "margin-top: 1em; margin-bottom: auto; margin-left: 1em; border: black solid 1px;"
+            })
+            .children(vec![
+              function_definition_arguments(vec!["props".to_string()])
+            ]),
+          El::new("div").attrs(hashmap! {
+            "style" => "flex: 1;"
+          }),
+          El::new("div")
+            .attrs(hashmap! {
+              "style" => "margin-top: auto; margin-bottom: 1em; margin-right: 1em; border: black solid 1px;"
+            })
+            .sink(returns.clone())
+            .children(vec![
+              function_definition_returns(vec!["view".to_string()])
+            ]),
+        ])
+    });
+
+    (view, msg)
+  }
+}
+
+fn function_definition_arguments(arguments: Vec<String>) -> El {
+  El::new("div")
+    .attrs(hashmap! {
+      "style" => "
+        margin-top: 1em;
+        margin-bottom: 1em;
+        margin-left: 0.9em;
+        margin-right: -0.5em;
+        background-color: white;
+      "
+    })
+    .children(arguments.iter().map(|arg| connection_stamen(arg)).collect())
+}
+
+fn function_definition_returns(returns: Vec<String>) -> El {
+  El::new("div")
+    .attrs(hashmap! {
+      "style" => "
+          margin-top: 1em;
+          margin-bottom: 1em;
+          margin-left: -0.5em;
+          margin-right: 0.9em;
+          background-color: white;
+        "
+    })
+    .children(returns.iter().map(|ret| connection_pistil(ret)).collect())
+}
+
+fn connection_stamen(arg: &str) -> El {
+  El::new("div").children(vec![
+    arg.into(),
+    El::new("div").attrs(hashmap! {
+      "style" => "width: 1em; height: 0px; border-top: black solid 1px; margin-top: 0.5em; margin-left: 0.5em;"
+    })
+  ])
+}
+
+fn connection_pistil(ret: &str) -> El {
+  El::new("div").children(vec![
+    El::new("div").attrs(hashmap! {
+      "style" => "width: 1em; height: 0px; border-top: black solid 1px; margin-top: 0.5em; margin-right: 0.5em;"
+    }),
+    ret.into(),
+  ])
+}
+
+// fn function_detail(function: Stream<Function>) -> Stream<El> {
+//   !
+// }
 
 fn app() -> Stream<El> {
-  let empty = Sink::new();
-  let main = initialize(&empty.stream(), Main { argument: None });
-  let overview = overview(main);
+  let mut call_graph = CallGraph::new();
+  let call_graph_stream = initialize(&Sink::new().stream(), call_graph);
+  let (overview_view, overview_msg) = function_overview::view(call_graph_stream);
 
-  overview.map(move |overview| {
-    El::new("div")
-      .attrs(hashmap! {
-        "style" => "display: flex;"
-      })
-      .children(vec![overview.into_owned()])
-  })
+  overview_view
 }
 
 #[wasm_bindgen]
