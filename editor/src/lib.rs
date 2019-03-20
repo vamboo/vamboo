02bf4fee2b6f11de3932bd12d0002b1411d75158
tock203 <tick@tock.tech>
2019-03-20 12:41:45 +0900

    hoge

--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -4,52 +4,65 @@ use frappe::{Sink, Stream};
 use frappe_dom::{initialize, mount, Element as El, EventsSink};
 use maplit::hashmap;
 use maybe_owned::MaybeOwned;
-use petgraph::graph::NodeIndex;
+use petgraph::graph::{EdgeIndex, NodeIndex};
 use petgraph::{Directed, Graph};
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 mod util;
+use lazy_static::lazy_static;
 use util::*;
 use wasm_bindgen::prelude::*;
 use web_sys::{window, MouseEvent};
+
+lazy_static! {
+  static ref ARGUMENTS_NODE_INDEX: NodeIndex = 1.into();
+  static ref RETURNS_NODE_INDEX: NodeIndex = 0.into();
+}
 
 #[derive(Clone)]
 enum RoughConnection {
   Between(NodeIndex, NodeIndex),
   // In case where connection ends is not yet decided
-  BeginWith(NodeIndex),
+  Begin,
   // In case where connection begins is not yet decided
-  EndWith(NodeIndex),
+  End,
 }
 
 // 多引数/多返り値を持つ関数を扱う際は()が引数<->返り値のマップになる
 #[derive(Clone)]
-struct CallGraph(Graph<String, (), Directed>);
+struct CallGraph(Graph<(), (), Directed>);
 
 impl CallGraph {
   fn new() -> Self {
     let mut call_graph = Graph::new();
-    call_graph.add_node("main".to_string());
+    call_graph.add_node(()); // returns
+    call_graph.add_node(()); // arguments
     Self(call_graph)
   }
 }
 
 impl Deref for CallGraph {
-  type Target = Graph<String, (), Directed>;
+  type Target = Graph<(), (), Directed>;
 
   fn deref(&self) -> &Self::Target {
     &self.0
   }
 }
 
+impl DerefMut for CallGraph {
+  fn deref_mut(&mut self) -> &mut Graph<(), (), Directed> {
+    &mut self.0
+  }
+}
+
 fn function_overview(call_graph: Stream<CallGraph>) -> (Stream<El>, Stream<app::Action>) {
   let mut arguments = EventsSink::new();
   let mut returns = EventsSink::new();
-  let focus_arguments = arguments.events("click").map(|_: MaybeOwned<MouseEvent>| {
-    app::Action::Focus(app::Display::RoughConnection(RoughConnection::BeginWith(1.into())))
-  });
-  let focus_returns = returns.events("click").map(|_: MaybeOwned<MouseEvent>| {
-    app::Action::Focus(app::Display::RoughConnection(RoughConnection::EndWith(0.into())))
-  });
+  let focus_arguments = arguments
+    .events("click")
+    .map(|_: MaybeOwned<MouseEvent>| app::Action::Focus(app::Display::RoughConnection(RoughConnection::Begin)));
+  let focus_returns = returns
+    .events("click")
+    .map(|_: MaybeOwned<MouseEvent>| app::Action::Focus(app::Display::RoughConnection(RoughConnection::End)));
   let action = focus_arguments.merge(&focus_returns);
 
   // TODO: call_graphを使って各GUI要素との繋がりを表示する
@@ -123,10 +136,69 @@ fn connection_pistil(ret: &str) -> El {
 }
 
 pub(crate) fn connection_detail(props: Stream<(CallGraph, RoughConnection)>) -> (Stream<El>, Stream<app::Action>) {
-  (
-    props.map(|_| El::new("div").children(vec!["Hello!".into()])),
-    Stream::never(),
-  )
+  let mut button = EventsSink::new();
+  let fork_returns = button
+    .events("click")
+    .map(|_: MaybeOwned<MouseEvent>| app::Action::ForkArguments(*RETURNS_NODE_INDEX));
+
+  let view = props.map(move |props| {
+    let (call_graph, focus) = props.into_owned();
+    match focus {
+      RoughConnection::Begin => unimplemented!(),
+      RoughConnection::End => El::new("div").children(vec![
+        El::new("div").attrs(hashmap! {
+          "style" => "margin-top: auto; margin-bottom: auto; border-top: cyan dashed 1px; flex: 1;"
+        }),
+        El::new("div")
+          .attrs(hashmap! {
+            "style" => "margin-top: auto; margin-bottom: auto; border: black solid 1px; position: relative;"
+          })
+          .children(vec![
+            function_definition_returns(vec!["view".to_string()]),
+            El::new("div")
+              .attrs(hashmap! {
+                "style" => "position: absolute; bottom: calc(-1px - 5px - 6px); left: calc(-1px - 5px - 6px);"
+              })
+              .children(vec![plus().sink(button.clone())]),
+          ]),
+        El::new("div").attrs(hashmap! {
+          "style" => "flex: 1;"
+        }),
+      ]),
+      RoughConnection::Between(_, _) => unimplemented!(),
+    }
+  });
+
+  (view, fork_returns)
+}
+
+fn plus() -> El {
+  El::new("div")
+    .attrs(hashmap! {
+      "style" => "position: relative; width: calc(13px + 10px); height: calc(13px + 10px); background-color: white;"
+    })
+    .children(vec![
+      El::new("div").attrs(hashmap! {
+        "style" => "
+          position: absolute;
+          border-top: black solid 1px;
+          left: 5px;
+          top: calc(5px + 6px);
+          height: 0px;
+          width: 13px;
+        "
+      }),
+      El::new("div").attrs(hashmap! {
+        "style" => "
+          position: absolute;
+          border-left: black solid 1px;
+          left: calc(5px + 6px);
+          top: 5px;
+          height: 13px;
+          width: 0px;
+        "
+      }),
+    ])
 }
 
 mod app {
@@ -156,6 +228,8 @@ mod app {
   #[derive(Clone)]
   pub(crate) enum Action {
     Focus(Display),
+    ForkArguments(NodeIndex),
+    ForkReturns(NodeIndex),
   }
 
   pub(crate) fn view() -> Stream<El> {
@@ -163,10 +237,16 @@ mod app {
     let state = action_sink
       .stream()
       .scan(State::new(), |mut state, action| match action.into_owned() {
+        Action::ForkArguments(function_call) => {
+          let new_function_call = state.call_graph.add_node(());
+          state.call_graph.add_edge(new_function_call, function_call, ());
+          state
+        }
         Action::Focus(display) => {
           state.display = display;
           state
         }
+        Action::ForkReturns(_) => unimplemented!(),
       });
     let state = initialize(&state, State::new());
 
