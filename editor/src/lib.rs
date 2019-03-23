0cc95bc297a7c6666390da4ba692daf9319c0ad6
tock203 <tick@tock.tech>
2019-03-23 18:52:01 +0900

    Port connection_detail too

--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -87,7 +87,11 @@ impl App {
     render();
   }
 
-  fn fork_arguments(&mut self, node_id: NodeIndex) {}
+  fn fork_arguments(&mut self, function_call: NodeIndex) {
+    let new_function_call = self.call_graph.add_node(());
+    self.call_graph.add_edge(new_function_call, function_call, ());
+    render();
+  }
 
   fn fork_returns(&mut self, node_id: NodeIndex) {}
 }
@@ -96,9 +100,91 @@ impl Render for App {
   fn render<'a: 'bump, 'bump>(&'a self, bump: &'bump Bump) -> Node<'bump> {
     match self.display.clone() {
       Display::Overview => function_overview(bump),
-      Display::RoughConnection(rough_connection) => div(bump).children([text("TODO")]).finish(),
+      Display::RoughConnection(displaying) => connection_detail(bump, &self.call_graph, displaying),
     }
   }
+}
+
+#[rustfmt::skip]
+fn connection_detail<'a: 'bump, 'bump>(
+  bump: &'bump Bump,
+  call_graph: &'a CallGraph,
+  displaying: RoughConnection
+) -> Node<'bump> {
+  match displaying {
+    RoughConnection::End => {
+      div(bump)
+      .children([
+        div(bump)
+        .attr("style", "
+margin-top: auto;
+margin-bottom: auto;
+border-top: cyan dashed 1px;
+flex: 1;")
+        .finish(),
+        div(bump)
+        .attr("style", "
+margin-top: auto;
+margin-bottom: auto;
+border: black solid 1px;
+position: relative;")
+        .children([
+          function_definition_returns(bump, "view"),
+          div(bump)
+          .attr("style", "
+position: absolute;
+bottom: calc(-1px - 5px - 6px);
+left: calc(-1px - 5px - 6px);")
+          .children([
+            div(bump)
+            .on("click", |root, _, _| root.unwrap_mut::<App>().fork_arguments(*RETURNS_NODE_INDEX))
+            .children([
+              plus(bump)
+            ])
+            .finish()
+          ])
+          .finish()
+        ])
+        .finish(),
+        div(bump)
+        .attr("style", "flex: 1;")
+        .finish()
+      ])
+      .finish()
+    }
+    _ => unimplemented!()
+  }
+}
+
+#[rustfmt::skip]
+fn plus<'bump>(bump: &'bump Bump) -> Node<'bump> {
+  div(bump)
+  .attr("style", "
+position: relative;
+width: calc(13px + 10px);
+height: calc(13px + 10px);
+background-color: white;")
+  .children([
+    div(bump)
+    .attr("style", "
+position: absolute;
+border-top: black solid 1px;
+left: 5px;
+top: calc(5px + 6px);
+height: 0px;
+width: 13px;")
+    .finish(),
+    div(bump)
+    .attr("style", "
+position: absolute;
+border-left: black solid 1px;
+left: calc(5px + 6px);
+top: 5px;
+height: 13px;
+width: 0px;")
+    .finish()
+  ])
+  .finish()
 }
 
 #[rustfmt::skip]
